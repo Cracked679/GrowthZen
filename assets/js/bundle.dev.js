1f5c08615cd0127500e57c2c39425e9f4adf5dc9
/ <31470255+Cracked679@users.noreply.github.com>
2020-10-23 00:23:27 +0530

    Initial Commit
    Still should improve some things.

---
+++ assets/js/bundle.dev.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// Typing Effect
+var typedTextSpan = document.querySelector(".typed-text");
+var cursorSpan = document.querySelector(".cursor");
+var textArray = ["Digital Marketing", "Web Design", "Web Development"];
+var typingDelay = 200;
+var erasingDelay = 100;
+var newTextDelay = 2000; // Delay between current and next text
+
+var textArrayIndex = 0;
+var charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+}); // Slider
